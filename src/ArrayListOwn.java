33950efd16a1dcf3cfb9c5c7c5cc27a72eab4bc4
OlegSsSs <114469359+OlegSsSs@users.noreply.github.com>
2023-11-26 00:05:26 +0300

    done

---
+++ src/ArrayListOwn.java
@@ -0,0 +1,212 @@
+import java.util.Arrays;
+
+/**
+ * ArrayList - own implementation ArrayList.
+ * Methods include:
+ * adding an element
+ * adding an element by index
+ * getting an element
+ * remove element
+ * clearing the collection
+ * sort bubble
+ *
+ * @param <E> type of elements in the collection
+ * @author OlegS
+ */
+public class ArrayListOwn<E> {
+
+    /**
+     * Capacity on default for elements array
+     */
+    private static final int DEFAULT_CAPACITY_ELEMENTS = 10;
+
+    /**
+     * Array to store elements
+     */
+    private Object[] arrayData;
+
+    /**
+     * Current size of list
+     */
+    private int size;
+
+    /**
+     * Creates an empty list with an initial capacity of 10
+     */
+    public ArrayListOwn() {
+        this.arrayData = new Object[DEFAULT_CAPACITY_ELEMENTS];
+        this.size = 0;
+    }
+
+    /**
+     * Adds an element to the end of the list
+     *
+     * @param element element to be added to the list
+     */
+    public void add(E element) {
+        checkAndIncreaseCapacity();
+        arrayData[size++] = element;
+    }
+
+    /**
+     * Adds an element for a specific index
+     *
+     * @param index index at which to add the element
+     * @param element element to be added to the list
+     */
+    public void addByIndex(int index, E element) {
+        checkIndex(index);
+        checkAndIncreaseCapacity();
+        System.arraycopy(arrayData, index, arrayData, index + 1, size - index);
+        arrayData[index] = element;
+        size++;
+    }
+
+    /**
+     * Returns the element at the specified index
+     *
+     * @param index index of the element to return
+     * @return element at the specified index
+     */
+    @SuppressWarnings("unchecked")
+    public E getByIndex(int index) {
+        checkIndex(index);
+        return (E) arrayData[index];
+    }
+
+    /**
+     * Remove element at the specified index
+     *
+     * @param index index of the element to remove
+     */
+    public void removeByIndex(int index) {
+        checkIndex(index);
+        System.arraycopy(arrayData, index + 1, arrayData, index, size - index - 1);
+        arrayData[--size] = null;
+    }
+
+    /**
+     * Clears the list by remove all elements
+     */
+    public void clear() {
+//        Arrays.fill(arrayData, null);
+        arrayData = new Object[DEFAULT_CAPACITY_ELEMENTS];
+        size = 0;
+    }
+
+    /**
+     * Returns the number of elements list
+     *
+     * @return number of elements in the collection
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Check if list is full, increases the capacity of the list by x2
+     */
+    private void checkAndIncreaseCapacity() {
+        if (size == arrayData.length) {
+            int newCapacity = arrayData.length * 2;
+            arrayData = Arrays.copyOf(arrayData, newCapacity);
+        }
+    }
+
+    /**
+     * Checks the specified index is valid for accessing list elements
+     *
+     * @param index index for check
+     * @throws IndexOutOfBoundsException if the specified index is outside the valid range
+     */
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "List: " + Arrays.toString(Arrays.copyOf(arrayData, size));
+    }
+
+    /**
+     * Compare elements in the list
+     *
+     * @param index1 the index of the first element to compare
+     * @param index2 the index of the second element to compare
+     * @return a negative integer if the first element is less than the second, zero if they are equal,
+     * and a positive integer if the first element is greater
+     */
+    @SuppressWarnings("unchecked")
+    private int compareByIndex(int index1, int index2) {
+        E element1 = (E) arrayData[index1];
+        E element2 = (E) arrayData[index2];
+        return Integer.compare(element1.hashCode(), element2.hashCode());
+    }
+
+    /**
+     * Swap two elements in the list
+     *
+     * @param index1 the index of the first element
+     * @param index2 the index of the second element
+     */
+    @SuppressWarnings("unchecked")
+    private void swap(int index1, int index2) {
+        E tmp = (E) arrayData[index1];
+        arrayData[index1] = arrayData[index2];
+        arrayData[index2] = tmp;
+    }
+
+    /**
+     * Sorts the elements of the list in their hashcode order using the Bubble Sort algorithm
+     */
+    public void sortBubble() {
+        int n = size;
+        boolean needIteration = true;
+        while (needIteration) {
+            needIteration = false;
+            for (int i = 1; i < n; i++) {
+                if (compareByIndex(i, i - 1) < compareByIndex(i - 1, i)) {
+                    swap(i, i - 1);
+                    needIteration = true;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        ArrayListOwn<Integer> list = new ArrayListOwn<>();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        System.out.println("List size after adding 3 elements: " + list.size());
+        System.out.println(list.toString());
+
+        list.addByIndex(1, 25);
+        System.out.println("List size after adding element " + list.getByIndex(1) + " at index 1: " + list.size());
+        System.out.println(list.toString());
+
+        int element = list.getByIndex(1);
+        System.out.println("Element at index 1: " + element);
+
+        list.removeByIndex(0);
+        System.out.println(list.toString() + " after delete element at index 0");
+
+        list.clear();
+        System.out.println("List size after clearing: " + list.size());
+
+        list.add(40);
+        list.add(6);
+        list.add(-21);
+        list.add(199);
+        System.out.println("Before use bubble sort " + list.toString());
+        list.sortBubble();
+        System.out.println("After sort " + list.toString());
+    }
+}
+
+
+
---
+++ src/LinkedListOwn.java
@@ -0,0 +1,267 @@
+/**
+ * LinkedList - own implementation LinkedList.
+ * Methods include:
+ * adding an element
+ * adding an element by index
+ * getting an element by index
+ * remove element by key and by index
+ * clearing the collection
+ * sort insertion
+ *
+ * @author OlegS
+ *
+ */
+
+public class LinkedListOwn {
+
+    /**
+     * Head of list
+     */
+    Node head;
+
+    /**
+     *  Linked list Node. Node is a static nested class
+     */
+    static class Node {
+        int data;
+        Node next;
+
+        public Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    /**
+     * Method to insert a new node
+     *
+     * @param list to which is added data
+     * @param data the int data to be added
+     */
+    public static void add(LinkedListOwn list, int data) {
+
+        Node newNode = new Node(data);
+        newNode.next = null;
+
+        if (list.head == null) {
+            list.head = newNode;
+        } else {
+            Node last = list.head;
+            while (last.next != null) {
+                last = last.next;
+            }
+            last.next = newNode;
+        }
+    }
+
+    /**
+     * Adds a data for a specific index
+     * @param list to which is added data
+     * @param index index at which to add the data
+     * @param data the int data to be added
+     */
+    public static void addByIndex(LinkedListOwn list, int index, int data) {
+
+        Node newNode = new Node(data);
+
+        if (index == 0) {
+            newNode.next = list.head;
+            list.head = newNode;
+        } else {
+            Node currNode = list.head;
+            int currentIndex = 0;
+            while (currNode != null && currentIndex < index - 1) {
+                currNode = currNode.next;
+                currentIndex++;
+            }
+            if (currNode == null) {
+                System.out.println("is out of bounds for the list");
+            } else {
+                newNode.next = currNode.next;
+                currNode.next = newNode;
+            }
+        }
+    }
+
+    /**
+     * Returns the data at the specified index
+     * @param list to which is added data
+     * @param index index at which to add the data
+     * @return returns the data at this index
+     * @throws IndexOutOfBoundsException if the specified index is outside the valid range
+     */
+    public static int getElementAtIndex(LinkedListOwn list, int index) {
+
+        Node currNode = list.head;
+        int currIndex = 0;
+
+        while (currNode != null) {
+            if (currIndex == index) {
+                return currNode.data;
+            }
+            currNode = currNode.next;
+            currIndex++;
+        }
+        throw new IndexOutOfBoundsException();
+    }
+
+    /**
+     * Displays a list
+     * @param list will be displayed on screen
+     */
+    public static void printList(LinkedListOwn list) {
+        Node currNode = list.head;
+        System.out.print("LinkedList: ");
+        while (currNode != null) {
+            System.out.print(currNode.data + " ");
+            currNode = currNode.next;
+        }
+        System.out.println();
+    }
+
+    /**
+     * Remove data at the value
+     * @param list to which is remove data
+     * @param value data to remove
+     */
+    public static void removeByValue(LinkedListOwn list, int value) {
+
+        Node currNode = list.head, prev = null;
+
+        if (currNode != null && currNode.data == value) {
+            list.head = currNode.next;
+            System.out.println(value + " value find and remove");
+            return;
+        }
+        while (currNode != null && currNode.data != value) {
+            prev = currNode;
+            currNode = currNode.next;
+        }
+        if (currNode != null) {
+            prev.next = currNode.next;
+            System.out.println(value + " value not found");
+        }
+        if (currNode == null) {
+            System.out.println(value + " value not found");
+        }
+    }
+
+    /**
+     * Remove data at the specified index
+     * @param list to which is remove data
+     * @param index index at which to remove the data
+     */
+    public static void removeByIndex(LinkedListOwn list, int index) {
+        Node currNode = list.head, prev = null;
+
+        if (index == 0 && currNode != null) {
+            list.head = currNode.next;
+            System.out.println("data at index: " + index + " remove");
+            return;
+        }
+        int counter = 0;
+        while (currNode != null) {
+            if (counter == index) {
+                prev.next = currNode.next;
+                System.out.println("data at index: " + index + " remove");
+                break;
+            } else {
+                prev = currNode;
+                currNode = currNode.next;
+                counter++;
+            }
+        }
+        if (currNode == null) {
+            System.out.println(index + " index not found");
+        }
+    }
+
+    /**
+     * Removes all data from a linked list
+     * @param list to which is need remove all data
+     */
+    public static void clear(LinkedListOwn list) {
+        list.head = null;
+    }
+
+    /**
+     * Sorts a linked list in ascending order using the insertion method
+     * @param list to which is need sorting
+     */
+    public static void sortList(LinkedListOwn list) {
+        Node currNode = list.head, prev = null;
+        int tmp;
+
+        if (list.head == null) {
+            System.out.println("List is empty");
+        } else {
+            while (currNode != null) {
+                prev = currNode.next;
+                while (prev != null) {
+                    if (currNode.data > prev.data) {
+                        tmp = currNode.data;
+                        currNode.data = prev.data;
+                        prev.data = tmp;
+                    }
+                    prev = prev.next;
+                }
+                currNode = currNode.next;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        LinkedListOwn list = new LinkedListOwn();
+
+        add(list, 1);
+        add(list, 2);
+        add(list, 3);
+        add(list, 4);
+        add(list, 5);
+        printList(list);
+        addByIndex(list, 2, 23);
+        printList(list);
+
+        System.out.println(getElementAtIndex(list, 2));
+
+        sortList(list);
+        printList(list);
+        clear(list);
+
+        LinkedListOwn list2 = new LinkedListOwn();
+
+        add(list2, 4);
+        add(list2, 2);
+        add(list2, 1);
+        add(list2, 3);
+        add(list2, 10);
+        printList(list2);
+
+        removeByValue(list2, 4);
+        printList(list2);
+
+        LinkedListOwn list1 = new LinkedListOwn();
+
+        add(list1, 1);
+        add(list1, 2);
+        add(list1, 3);
+        add(list1, 4);
+        add(list1, 5);
+        add(list1, 6);
+        add(list1, 7);
+        add(list1, 8);
+        printList(list1);
+
+        removeByIndex(list1, 0);
+        printList(list1);
+
+        removeByIndex(list1, 2);
+        printList(list1);
+
+        removeByIndex(list1, 10);
+        printList(list1);
+    }
+}
+
+
